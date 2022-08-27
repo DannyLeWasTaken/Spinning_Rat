5df5018eefcbe8ed21ec0281679bfe79ca42be55
DannyLeWasTaken <dannylewastaken@protonmail.com>
2022-08-27 19:12:08 -0400

    Added getters and setters

--- src/camera.cpp
+++ src/camera.cpp
@@ -12,5 +12,14 @@
 
 Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch): Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(16.f), MouseSensitivity(45.f)
 {
-
+    Position = position;
+    WorldUp = up;
+    Yaw = yaw;
+    Pitch = pitch;
 }
+
+glm::mat4 Camera::GetViewMatrix()
+{
+    return glm::lookAt(Position, Position + Front, Up);
+}
+
--- src/camera.hpp
+++ src/camera.hpp
@@ -16,6 +16,25 @@ public:
             glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
             float yaw = -90.0f,
             float pitch = 0.0f);
+
+    glm::mat4 GetViewMatrix();
+
+    void UpdateCameraVectors();
+
+    // Getters and setters
+    glm::vec3 GetPosition() const { return Position; }
+    glm::vec3 GetFrontVector() const { return Front; }
+    glm::vec3 GetUpVector() const { return Up; }
+    glm::vec3 GetRightVector() const { return Right; }
+    glm::vec3 GetWorldUpVector() const { return WorldUp; }
+
+    float GetYaw() const { return Yaw; }
+    float GetPitch() const { return Pitch; }
+    float GetRoll() const { return Roll; }
+    float GetMovementSpeed() const { return MovementSpeed; }
+    float GetMouseSensitivity() const { return MouseSensitivity; }
+    float GetFOV() const { return FOV; }
+
 private:
     glm::vec3 Position, Front, Up, Right, WorldUp;
     float Yaw, Pitch, Roll, MovementSpeed, MouseSensitivity, FOV;
