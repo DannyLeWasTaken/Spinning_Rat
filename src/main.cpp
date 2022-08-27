8b94db863fea261fda4bb3729a963a85cf670eee
DannyLeWasTaken <dannylewastaken@protonmail.com>
2022-08-27 13:01:20 -0400

    *free bird blasts in the background*

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+
+#include <glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int SCR_WIDTH = 800, SCR_HEIGHT = 600;
+double DeltaTime;
+double LastFrameTime;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Spinning Rat", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    unsigned int VAO, VBO;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        {
+            double time = glfwGetTime();
+            DeltaTime = time - LastFrameTime;
+            LastFrameTime = time;
+        }
+
+        processInput(window);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0,0,width,height);
+    SCR_WIDTH = width;
+    SCR_HEIGHT = height;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE)==GLFW_PRESS)
+        glfwTerminate();
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+}
